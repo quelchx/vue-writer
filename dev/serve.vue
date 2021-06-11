df54951d9851c6be61be44c5aa41a5e329c1a851
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2021-06-11 15:17:42 -0400

    project init --testing phases

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import VueWriter from '@/vue-writer.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueWriter
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-writer :array="['Hello World']"/>
+  </div>
+</template>
---
+++ src/vue-writer.vue
@@ -0,0 +1,106 @@
+<script>
+import { defineComponent } from "vue";
+
+export default /*#__PURE__*/ defineComponent({
+  name: "VueWriter", // vue component name
+  props: {
+    array: {
+      type: Array,
+      required: true,
+    },
+    eraseSpeed: {
+      type: Number,
+      default: 100,
+    },
+    typeSpeed: {
+      type: Number,
+      default: 200,
+    },
+    delay: {
+      type: Number,
+      default: 2000,
+    },
+  },
+  data() {
+    return {
+      typeValue: "",
+      typeStatus: false,
+      arrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  methods: {
+    typewriter() {
+      if (this.charIndex < this.array[this.arrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.array[this.arrayIndex].charAt(this.charIndex);
+        this.charIndex += 1;
+        setTimeout(this.typewriter, this.typeSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraser, this.delay);
+      }
+    },
+    eraser() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.array[this.arrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+        setTimeout(this.eraser, this.eraseSpeed);
+      } else {
+        this.typeStatus = false;
+        this.arrayIndex += 1;
+        if (this.arrayIndex >= this.array.length) this.arrayIndex = 0;
+        setTimeout(this.typewriter, this.typeSpeed + 1000);
+      }
+    },
+  },
+  created() {
+    setTimeout(this.typewriter, this.delay + 200);
+  },
+});
+</script>
+
+<template>
+  <div class="is-typed">
+    <slot />
+    <span class="typed">{{ typeValue }}</span>
+    <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+  </div>
+</template>
+
+<style>
+.is-typed {
+  font-family: "Monaco";
+}
+
+.is-typed span.typed {
+  color: steelblue;
+}
+
+.is-typed span.cursor {
+  display: inline-block;
+  width: 4px;
+  background-color: black;
+  animation: blink 1s infinite;
+}
+
+.is-typed span.cursor.typing {
+  animation: none;
+}
+
+@keyframes blink {
+  49% {
+    background-color: black;
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
+}
+</style>
